0c40db3b287d4a9147de142bb231fc07bc12eef6
Airshock72 <gelagela2222@gmail.com>
2025-12-14 12:18:59 +0400

    implemented nav bar with animation effects and styles

---
+++ constants.ts
@@ -0,0 +1,24 @@
+
+interface NavLinks {
+    readonly id: string
+    readonly title: string
+}
+
+export const navLinks: Array<NavLinks> = [
+  {
+    id: 'cocktails',
+    title: 'Cocktails'
+  },
+  {
+    id: 'about',
+    title: 'About Us'
+  },
+  {
+    id: 'work',
+    title: 'The Art'
+  },
+  {
+    id: 'contact',
+    title: 'Contact'
+  }
+]
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,44 @@
+import { navLinks } from '../../constants.ts'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const NavBar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: 'nav',
+      start: 'bottom top'
+    })
+
+    navTween.fromTo(
+      'nav',
+      { backgroundColor: 'transparent' },
+      {
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut'
+      }
+    )
+  })
+
+  return (
+    <nav>
+      <div>
+        <a href='#home' className='flex items-center gap-2'>
+          <img src='/images/logo.png' alt='Logo' />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link, index) => (
+            <li key={`${link.id}-${index}`}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
