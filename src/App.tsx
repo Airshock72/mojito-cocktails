dfe270ea183bf8c0254e2efdd9689bcdf8d0e0d8
Airshock72 <gelagela2222@gmail.com>
2025-12-14 23:32:09 +0400

    implemented about page with animations and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import NavBar from './components/NavBar.tsx'
 import Hero from './components/Hero.tsx'
 import Cocktails from './components/Cocktails.tsx'
+import About from './components/About.tsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -12,6 +13,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
